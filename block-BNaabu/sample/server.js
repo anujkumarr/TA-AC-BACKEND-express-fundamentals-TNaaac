a6a8bfd2327783f81b0290ebe5b659d79dee298f
akk26 <anujbph@gmail.com>
2021-10-31 13:22:44 +0530

    completed

---
+++ block-BNaabu/sample/server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var logger = require("morgan");
+var cookieParser = require("cookies-parser");
+
+var app = express();
+
+// middleware
+
+app.use(express.json());
+
+
+
+app.use('/admin', (req, res, next) => {
+  next('Unauthorised request');
+});
+
+app.get('/', (req, res, next) => {
+  res.send('Welcome');
+  next();
+});
+
+app.get('/about', (req, res, next) => {
+  res.send('Welcome to the about page');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("This requested url is not found")
+  next();
+})
+
+app.use((err, req, res, next) => {
+  res.send(err);
+})
+
+
+// server
+
+app.listen(1234, () => {
+  console.log("Server is listening on port 1234");
+})
